f7eaa8f8efa90bf2d825d71362f9ea9702e5af48
patternnoster <retsonnrettap@gmail.com>
2023-08-10 00:37:56 +0300

    Basic os_resource interface

---
+++ include/memaw/os_resource.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <cstddef>
+
+namespace memaw {
+
+using std::size_t;
+
+/**
+ * @brief Memory resource that always allocates and frees memory via
+ *        direct system calls to the OS
+ **/
+class os_resource {
+public:
+  constexpr os_resource() noexcept = default;
+
+  [[nodiscard]] static void* allocate
+    (size_t size, size_t alignment = alignof(std::max_align_t)) noexcept;
+
+  static void deallocate(void* ptr, size_t size,
+                         size_t alignment = alignof(std::max_align_t)) noexcept;
+
+  constexpr bool operator==(const os_resource&) const noexcept = default;
+};
+
+} // namespace memaw
