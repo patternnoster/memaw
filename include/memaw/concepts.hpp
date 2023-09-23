689e62c8c563b1c320b01db40d5b88c00e86c0f5
patternnoster <retsonnrettap@gmail.com>
2023-09-23 20:37:41 +0300

    The sweeping_resource concept & enable_sweeping_resource

--- include/memaw/concepts.hpp
+++ include/memaw/concepts.hpp
@@ -95,6 +95,40 @@ concept overaligning_resource = resource<R> && requires() {
 
 /**
  * @brief A specializable global constant that enables the
+ *        sweeping_resource concept (see below) for R. By default,
+ *        true iff R defines a constexpr member R::is_sweeping
+ *        implicitly convertible to true
+ **/
+template <resource R>
+constexpr bool enable_sweeping_resource = requires {
+  { std::bool_constant<R::is_sweeping>{} } -> std::same_as<std::true_type>;
+};
+
+/**
+ * @brief The concept of a resource that permits combining
+ *        deallocations of adjacent regions into one call
+ *
+ * More formally, r is a sweeping resource if and only if for any two
+ * valid (see above) calls r.deallocate(ptr1, size1, alignment1) and
+ * r.deallocate(ptr2, size2, alignment2), if (ptr2 == (byte*)ptr1 +
+ * size1) then the call r.deallocate(ptr1, size1 + size2, alignment1)
+ * is also valid and has the same effect as the two former calls (in
+ * some order).
+ *
+ * To mark resource as sweeping one has to specialize the constant
+ * enable_sweeping_resource to true. If then the above condition is
+ * not satisfied, the behaviour of the program is undefined.
+ *
+ * @note If a resource never returns adjacent regions from allocate()
+ *       (and is not interchangeable with any resource that does) then
+ *       it is automatically sweeping (since no two deallocate() calls
+ *       for adjacent regions are valid in the first place)
+ **/
+template <typename R>
+concept sweeping_resource = resource<R> && enable_sweeping_resource<R>;
+
+/**
+ * @brief A specializable global constant that enables the
  *        thread_safe_resource concept (see below) for R. By default,
  *        true iff R defines a constexpr member R::is_thread_safe
  *        implicitly convertible to true
