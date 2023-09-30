ababa04226063d994fa11f64f885cf89626cf291
patternnoster <retsonnrettap@gmail.com>
2023-09-30 17:40:25 +0300

    Tests for pages_resource & aliases

--- test/concepts_tests.cpp
+++ test/concepts_tests.cpp
@@ -2,20 +2,33 @@
 #include <memory_resource>
 
 #include "memaw/concepts.hpp"
+#include "memaw/literals.hpp"
 #include "memaw/os_resource.hpp"
+#include "memaw/pages_resource.hpp"
 
 using namespace memaw;
 
-TEST(ConceptsTests, resource) {
+TEST(ConceptsTests, std_pmr) {
   EXPECT_TRUE(resource<std::pmr::synchronized_pool_resource>);
   EXPECT_TRUE(resource<std::pmr::unsynchronized_pool_resource>);
   EXPECT_TRUE(resource<std::pmr::monotonic_buffer_resource>);
+}
 
-  EXPECT_TRUE(resource<os_resource>);
-  EXPECT_TRUE(bound_resource<os_resource>);
-  EXPECT_TRUE(granular_resource<os_resource>);
-  EXPECT_TRUE(overaligning_resource<os_resource>);
-  EXPECT_TRUE(sweeping_resource<os_resource>);
-  EXPECT_TRUE(thread_safe_resource<os_resource>);
-  EXPECT_TRUE(nothrow_resource<os_resource>);
+template <class T>
+class PagesResourcesTests: public ::testing::Test {};
+
+using PagesResources = ::testing::Types<os_resource,
+                                        regular_pages_resource,
+                                        big_pages_resource,
+                                        fixed_pages_resource<2_MiB>>;
+TYPED_TEST_SUITE(PagesResourcesTests, PagesResources);
+
+TYPED_TEST(PagesResourcesTests, concepts) {
+  EXPECT_TRUE(resource<TypeParam>);
+  EXPECT_TRUE(bound_resource<TypeParam>);
+  EXPECT_TRUE(granular_resource<TypeParam>);
+  EXPECT_TRUE(overaligning_resource<TypeParam>);
+  EXPECT_TRUE(sweeping_resource<TypeParam>);
+  EXPECT_TRUE(thread_safe_resource<TypeParam>);
+  EXPECT_TRUE(nothrow_resource<TypeParam>);
 }
--- test/resource_tests.cpp
+++ test/resource_tests.cpp
@@ -7,6 +7,7 @@
 
 #include "memaw/literals.hpp"
 #include "memaw/os_resource.hpp"
+#include "memaw/pages_resource.hpp"
 
 using namespace memaw;
 
@@ -196,4 +197,32 @@ TEST_F(OsResourceTests, explicitly_sized_pages) {
     routine(pow2_t{size});
     break;
   }
+}
+
+TEST(PagesResourceTests, base) {
+  constexpr auto size = 2_MiB;
+
+  os_resource os;
+
+  regular_pages_resource reg;
+  big_pages_resource big;
+  fixed_pages_resource<size> fixed;
+
+  EXPECT_EQ(reg.min_size(), os.get_page_size());
+  EXPECT_EQ(big.min_size(), os.get_big_page_size().value_or(reg.min_size()));
+  EXPECT_EQ(fixed.min_size(), size);
+
+  EXPECT_GE(reg.guaranteed_alignment(), reg.min_size());
+  EXPECT_GE(big.guaranteed_alignment(), big.min_size());
+  EXPECT_GE(fixed.guaranteed_alignment(), fixed.min_size());
+
+  const auto reg_ptr = reg.allocate(reg.min_size());
+  EXPECT_NE(reg_ptr, nullptr);
+
+  const auto big_ptr = big.allocate(big.min_size());
+  const auto fixed_ptr = fixed.allocate(fixed.min_size());
+
+  reg.deallocate(reg_ptr, reg.min_size());
+  big.deallocate(big_ptr, big.min_size());
+  fixed.deallocate(fixed_ptr, fixed.min_size());
 }
