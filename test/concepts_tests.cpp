cdda699f02f0db3dd106965f374ac735d38979b1
patternnoster <retsonnrettap@gmail.com>
2023-10-27 00:39:24 +0300

    Concepts test for cache_resource

--- test/concepts_tests.cpp
+++ test/concepts_tests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <memory_resource>
 
+#include "memaw/cache_resource.hpp"
 #include "memaw/chain_resource.hpp"
 #include "memaw/concepts.hpp"
 #include "memaw/literals.hpp"
@@ -112,4 +113,57 @@ TEST(ChainResourceTests, concepts) {
 
   EXPECT_TRUE((substitutable_resource_for<mock_groups<0, 0>, chain_t<2>>));
   EXPECT_FALSE((substitutable_resource_for<chain_t<2>, mock_groups<2, 2>>));
+}
+
+using upstream1_t =
+  test_resource<resource_params{ .min_size = 4_KiB, .alignment = 8_KiB,
+                                 .is_sweeping = true, .group = {1, 1}}>;
+using upstream2_t =
+  test_resource<resource_params{ .nothrow_alloc = true, .nothrow_dealloc = true,
+                                 .is_sweeping = true, .is_thread_safe = true }>;
+
+template <resource U>
+using cache1_t =
+  cache_resource<cache_resource_config_t<U>{ .granularity = pow2_t{1_KiB} }>;
+
+template <resource U>
+using cache2_t =
+  cache_resource<cache_resource_config_t<U>{ .granularity = pow2_t{32} }>;
+
+using res1_t = cache1_t<upstream1_t>;
+using res2_t = cache1_t<upstream2_t>;
+using res3_t = cache2_t<upstream1_t>;
+using res4_t = cache2_t<upstream2_t>;
+
+template <typename T>
+class CacheResourceConceptsTests: public testing::Test {};
+using CacheResources = testing::Types<res1_t, res2_t, res3_t, res4_t>;
+TYPED_TEST_SUITE(CacheResourceConceptsTests, CacheResources);
+
+TYPED_TEST(CacheResourceConceptsTests, concepts) {
+  using upstream = TypeParam::upstream_t;
+
+  EXPECT_TRUE(resource<TypeParam>);
+  EXPECT_TRUE(nothrow_resource<TypeParam>);
+  EXPECT_TRUE(bound_resource<TypeParam>);
+  EXPECT_TRUE(granular_resource<TypeParam>);
+  EXPECT_TRUE(sweeping_resource<TypeParam>);
+
+  if constexpr (std::same_as<upstream, upstream1_t>) {
+    EXPECT_FALSE(thread_safe_resource<TypeParam>);
+    EXPECT_FALSE((substitutable_resource_for<TypeParam, upstream2_t>));
+    EXPECT_TRUE(overaligning_resource<TypeParam>);
+  }
+  else {
+    EXPECT_TRUE(thread_safe_resource<TypeParam>);
+    EXPECT_TRUE((substitutable_resource_for<TypeParam, upstream2_t>));
+    EXPECT_FALSE(overaligning_resource<TypeParam>);
+  }
+
+  EXPECT_FALSE((substitutable_resource_for<TypeParam, res1_t>));
+  EXPECT_FALSE((substitutable_resource_for<TypeParam, res2_t>));
+  EXPECT_FALSE((substitutable_resource_for<TypeParam, res3_t>));
+  EXPECT_FALSE((substitutable_resource_for<TypeParam, res4_t>));
+
+  EXPECT_TRUE((substitutable_resource_for<TypeParam, upstream1_t>));
 }
