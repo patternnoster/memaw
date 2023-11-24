c62fe6782a28d9a506788146b9c9c12b5782c9f3
patternnoster <retsonnrettap@gmail.com>
2023-11-24 22:26:20 +0300

    Tests reorganization: pages_resource concepts

--- test/concepts_tests.cpp
+++ test/concepts_tests.cpp
@@ -5,8 +5,6 @@
 #include "memaw/chain_resource.hpp"
 #include "memaw/concepts.hpp"
 #include "memaw/literals.hpp"
-#include "memaw/os_resource.hpp"
-#include "memaw/pages_resource.hpp"
 
 #include "test_resource.hpp"
 
@@ -16,33 +14,6 @@ TEST(ConceptsTests, std_pmr) {
   EXPECT_TRUE(resource<std::pmr::synchronized_pool_resource>);
   EXPECT_TRUE(resource<std::pmr::unsynchronized_pool_resource>);
   EXPECT_TRUE(resource<std::pmr::monotonic_buffer_resource>);
-}
-
-template <class T>
-class PagesResourcesTests: public ::testing::Test {};
-
-using PagesResources = ::testing::Types<os_resource,
-                                        regular_pages_resource,
-                                        big_pages_resource,
-                                        fixed_pages_resource<2_MiB>>;
-TYPED_TEST_SUITE(PagesResourcesTests, PagesResources);
-
-TYPED_TEST(PagesResourcesTests, concepts) {
-  EXPECT_TRUE(resource<TypeParam>);
-  EXPECT_TRUE(bound_resource<TypeParam>);
-  EXPECT_TRUE(granular_resource<TypeParam>);
-  EXPECT_TRUE(overaligning_resource<TypeParam>);
-  EXPECT_TRUE(sweeping_resource<TypeParam>);
-  EXPECT_TRUE(thread_safe_resource<TypeParam>);
-  EXPECT_TRUE(nothrow_resource<TypeParam>);
-
-  []<typename... Ts>(const testing::Types<Ts...>) {
-    const auto test = []<typename T>() {
-      EXPECT_TRUE((interchangeable_resource_with<TypeParam, T>));
-      EXPECT_TRUE((substitutable_resource_for<TypeParam, T>));
-    };
-    (test.template operator()<Ts>(), ...);
-  }(PagesResources{});
 }
 
 template <int _int, int _sub, size_t _idx = 0>
--- test/pages_resource_tests.cpp
+++ test/pages_resource_tests.cpp
@@ -7,6 +7,33 @@
 
 using namespace memaw;
 
+template <class T>
+class OsAndPagesResourcesTests: public ::testing::Test {};
+
+using OsAndPagesResources = ::testing::Types<os_resource,
+                                             regular_pages_resource,
+                                             big_pages_resource,
+                                             fixed_pages_resource<2_MiB>>;
+TYPED_TEST_SUITE(OsAndPagesResourcesTests, OsAndPagesResources);
+
+TYPED_TEST(OsAndPagesResourcesTests, concepts) {
+  EXPECT_TRUE(resource<TypeParam>);
+  EXPECT_TRUE(bound_resource<TypeParam>);
+  EXPECT_TRUE(granular_resource<TypeParam>);
+  EXPECT_TRUE(overaligning_resource<TypeParam>);
+  EXPECT_TRUE(sweeping_resource<TypeParam>);
+  EXPECT_TRUE(thread_safe_resource<TypeParam>);
+  EXPECT_TRUE(nothrow_resource<TypeParam>);
+
+  []<typename... Ts>(const testing::Types<Ts...>) {
+    const auto test = []<typename T>() {
+      EXPECT_TRUE((interchangeable_resource_with<TypeParam, T>));
+      EXPECT_TRUE((substitutable_resource_for<TypeParam, T>));
+    };
+    (test.template operator()<Ts>(), ...);
+  }(OsAndPagesResources{});
+}
+
 TEST(PagesResourceTests, base) {
   constexpr auto size = 2_MiB;
 
