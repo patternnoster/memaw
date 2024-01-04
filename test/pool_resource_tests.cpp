61cfe87e3909f5d074b2f8912bcbef37b6886abd
patternnoster <retsonnrettap@gmail.com>
2024-01-05 00:18:46 +0300

    The pool_resource test base & cases

--- test/pool_resource_tests.cpp
+++ test/pool_resource_tests.cpp
@@ -1,9 +1,11 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <memory>
 
 #include "memaw/literals.hpp"
 #include "memaw/pool_resource.hpp"
 
+#include "resource_test_base.hpp"
 #include "test_resource.hpp"
 
 using namespace memaw;
@@ -34,3 +36,68 @@ TEST(PoolResourceStaticTests, chunk_sizes) {
                                         .chunk_size_multiplier = 1 }>;
   EXPECT_THAT(pr3_t::chunk_sizes, ElementsAre(1_KiB));
 }
+
+
+using upstream1_t =  // Simple no limits
+  test_resource<resource_params{ .is_sweeping = true }>;
+
+using upstream2_t =  // Classical
+  test_resource<resource_params{ .min_size = 4_KiB, .alignment = 4_KiB,
+                                 .is_granular = true, .is_sweeping = true}>;
+
+using upstream3_t =  // Non-granular
+  test_resource<resource_params{ .min_size = 40000, .is_sweeping = true}>;
+
+using upstream4_t =  // Weird limits
+  test_resource<resource_params{ .min_size = 1000, .alignment = 32,
+                                 .is_granular = true, .is_sweeping = true}>;
+
+template <bool _thread_safe>
+constexpr pool_resource_config pool1_config = {
+  .min_chunk_size = pow2_t{1024},
+  .max_chunk_size = 8192,
+  .chunk_size_multiplier = 2,
+  .thread_safe = _thread_safe
+};
+
+template <bool _thread_safe>
+constexpr pool_resource_config pool2_config = {
+  .min_chunk_size = pow2_t{128},
+  .max_chunk_size = 10368,
+  .chunk_size_multiplier = 3,
+  .thread_safe = _thread_safe
+};
+
+template <resource R, bool _thread_safe = true>
+using pool1_t = pool_resource<R, pool1_config<_thread_safe>>;
+
+template <resource R, bool _thread_safe = true>
+using pool2_t = pool_resource<R, pool2_config<_thread_safe>>;
+
+template <typename T>
+class PoolResourceTestsBase: public testing::Test {
+protected:
+  using upstream_t = T::upstream_t;
+
+  void SetUp() {
+    test_pool = std::make_shared<T>(mock);
+  }
+
+  mock_resource mock;
+  std::shared_ptr<T> test_pool;
+};
+
+template <typename T>
+class PoolResourceTests: public resource_test,
+                         public PoolResourceTestsBase<T> {};
+
+using PoolResources =
+  testing::Types<pool1_t<upstream1_t>, pool1_t<upstream2_t>,
+                 pool1_t<upstream3_t>, pool1_t<upstream4_t>,
+                 pool2_t<upstream1_t>, pool2_t<upstream2_t>,
+                 pool2_t<upstream3_t>, pool2_t<upstream4_t>,
+                 pool1_t<upstream1_t, false>, pool1_t<upstream2_t, false>,
+                 pool1_t<upstream3_t, false>, pool1_t<upstream4_t, false>,
+                 pool2_t<upstream1_t, false>, pool2_t<upstream2_t, false>,
+                 pool2_t<upstream3_t, false>, pool2_t<upstream4_t, false>>;
+TYPED_TEST_SUITE(PoolResourceTests, PoolResources);
